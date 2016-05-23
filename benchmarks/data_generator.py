ffda955390b1e83c3b96abe0b0198c44884f6120
Paul <paulgc@cs.wisc.edu>
2016-05-22 19:14:51 -0500

    Added benchmark tests for join.

--- benchmarks/data_generator.py
+++ benchmarks/data_generator.py
@@ -1,65 +1,39 @@
+"""Utilities to generate synthetic data"""
 import random
 import string
 
 import pandas as pd
 
 
-def generate_tokens(length_distribution, num_tokens):
-    lengths = []
-    cum_prob = []
-    i = 0
-    for length in length_distribution:
-        lengths.append(length)
-        if i == 0:
-            cum_prob.append(length_distribution[length])
-        else:
-            cum_prob.append(length_distribution[length] + cum_prob[i-1])
-        i += 1
+def generate_tokens(mean, std_dev, num_tokens):
     tokens = {}
     cnt = 0
     while cnt < num_tokens:
-        rand = random.random()
-        selected_length = -1
-        for i in range(len(cum_prob)):
-            if rand <= cum_prob[i]:
-                selected_length = lengths[i]
-                break
+        length = int(round(random.normalvariate(mean,
+                                                std_dev)))
+        if length < 2:
+            continue
         flag = True
         while flag:
             new_token = ''.join(random.choice(string.ascii_lowercase)
-                                for i in range(selected_length))
-            if tokens.get(new_token) == None:
+                                for i in range(length))
+            if tokens.get(new_token) is None:
                 tokens[new_token] = True
                 flag = False
         cnt += 1
     return list(tokens.keys())
 
 
-def generate_table(size_distribution, tokens, num_records,
+def generate_table(mean, std_dev, tokens, num_records,
                    id_col_name, attr_col_name):
-    sizes = []
-    cum_prob = []
-    i = 0
-    for size in size_distribution:
-        sizes.append(size)
-        if i == 0:
-            cum_prob.append(size_distribution[size])
-        else:
-            cum_prob.append(size_distribution[size] + cum_prob[i-1])
-        i += 1
-
     records = []
     cnt = 0
     num_tokens = len(tokens)
     while cnt < num_records:
-        rand = random.random()
-        selected_size = -1
-        for i in range(len(cum_prob)):
-            if rand <= cum_prob[i]:
-                selected_size = sizes[i]
-                break
+        size = int(round(random.normalvariate(mean,
+                                              std_dev)))
         new_string = ''
-        for i in range(selected_size):
+        for i in range(size):
             rand = random.randint(0, num_tokens - 1)
             if i == 0:
                 new_string += tokens[rand]
--- benchmarks/join_benchmark.py
+++ benchmarks/join_benchmark.py
@@ -1,28 +1,92 @@
+"""Benchmarks for join methods"""
+
 from .data_generator import generate_table
 from .data_generator import generate_tokens  
-from py_stringsimjoin.join.join import jaccard_join
-from py_stringsimjoin.utils.tokenizers import create_delimiter_tokenizer
+from py_stringsimjoin.join.join import cosine_join, jaccard_join, \
+                                       overlap_join
+from py_stringsimjoin.utils.tokenizers import create_qgram_tokenizer, \
+                                           create_delimiter_tokenizer
 
-TOKEN_LENGTH_DISTRIBUTION = {3: 0.2,
-                             4: 0.2,
-                             5: 0.2,
-                             6: 0.2,
-                             7: 0.2}
 
-STRING_SIZE_DISTRIBUTION = {3: 0.2,
-                            4: 0.2,
-                            5: 0.2,
-                            6: 0.2,
-                            7: 0.2}
+class SmallJoinBenchmark:
+    """Small benchmark 10K x 10K"""
+    def setup(self):
+        tokens = generate_tokens(6, 2, 5000)
+        self.ltable = generate_table(5, 1, tokens,
+                                     10000, 'id', 'attr')
+        self.rtable = generate_table(5, 1, tokens,
+                                     10000, 'id', 'attr')
 
-NUM_TOKENS = 500
-TOKENS = generate_tokens(TOKEN_LENGTH_DISTRIBUTION, NUM_TOKENS)
-NUM_RECORDS = 10000
-LTABLE = generate_table(STRING_SIZE_DISTRIBUTION, TOKENS, NUM_RECORDS,
-                       'id', 'attr')
-RTABLE = LTABLE
-
-class JaccardJoinBenchmark:
-    def time_small_small_delim(self):
+    def time_jaccard_delim_07(self):
         dl = create_delimiter_tokenizer()
-        jaccard_join(LTABLE, RTABLE, 'id', 'id', 'attr', 'attr', dl, 0.8)
+        jaccard_join(self.ltable, self.rtable,
+                     'id', 'id', 'attr', 'attr',
+                     dl, 0.7)
+
+    def time_cosine_delim_07(self):
+        dl = create_delimiter_tokenizer()
+        cosine_join(self.ltable, self.rtable,
+                    'id', 'id', 'attr', 'attr',
+                    dl, 0.7)
+
+    def time_overlap_delim_1(self):
+        dl = create_delimiter_tokenizer()
+        overlap_join(self.ltable, self.rtable,
+                     'id', 'id', 'attr', 'attr',
+                     dl, 1)
+
+
+class MediumJoinBenchmark:
+    """Medium benchmark 25K x 25K"""
+    def setup(self):
+        tokens = generate_tokens(6, 2, 5000)
+        self.ltable = generate_table(5, 1, tokens,
+                                     25000, 'id', 'attr')
+        self.rtable = generate_table(5, 1, tokens,
+                                     25000, 'id', 'attr')
+
+    def time_jaccard_delim_07(self):
+        dl = create_delimiter_tokenizer()
+        jaccard_join(self.ltable, self.rtable,
+                     'id', 'id', 'attr', 'attr',
+                     dl, 0.7)
+
+    def time_cosine_delim_07(self):
+        dl = create_delimiter_tokenizer()
+        cosine_join(self.ltable, self.rtable,
+                    'id', 'id', 'attr', 'attr',
+                    dl, 0.7)
+
+    def time_overlap_delim_1(self):
+        dl = create_delimiter_tokenizer()
+        overlap_join(self.ltable, self.rtable,
+                     'id', 'id', 'attr', 'attr',
+                     dl, 1)
+
+
+class LargeJoinBenchmark:
+    """Large benchmark 50K x 50K"""
+    def setup(self):
+        tokens = generate_tokens(6, 2, 5000)
+        self.ltable = generate_table(5, 1, tokens,
+                                     50000, 'id', 'attr')
+        self.rtable = generate_table(5, 1, tokens,
+                                     50000, 'id', 'attr')
+
+    def time_jaccard_delim_07(self):
+        dl = create_delimiter_tokenizer()
+        jaccard_join(self.ltable, self.rtable,
+                     'id', 'id', 'attr', 'attr',
+                     dl, 0.7)
+
+    def time_cosine_delim_07(self):
+        dl = create_delimiter_tokenizer()
+        cosine_join(self.ltable, self.rtable,
+                    'id', 'id', 'attr', 'attr',
+                    dl, 0.7)
+
+    def time_overlap_delim_1(self):
+        dl = create_delimiter_tokenizer()
+        overlap_join(self.ltable, self.rtable,
+                     'id', 'id', 'attr', 'attr',
+                     dl, 1)
