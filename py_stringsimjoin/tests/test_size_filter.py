45f3da104f78e8455fae47b6af009531c8657cb7
Anson-Doan <anson.ddoan@gmail.com>
2023-03-15 17:47:17 -0500

    Update test_size_filter.py

--- py_stringsimjoin/tests/test_size_filter.py
+++ py_stringsimjoin/tests/test_size_filter.py
@@ -123,7 +123,7 @@ class FilterPairTestCases(unittest.TestCase):
         size_filter = SizeFilter(tokenizer, sim_measure_type, threshold,
                                  allow_empty, allow_missing)
         actual_output = size_filter.filter_pair(lstring, rstring)
-        np.assertEqual(actual_output, expected_output)
+        self.assertEqual(actual_output, expected_output)
 
 
 # test SizeFilter.filter_tables method
@@ -344,7 +344,7 @@ class FilterTablesTestCases(unittest.TestCase):
                     expected_output_attrs.append(r_out_prefix + attr)
 
         # verify whether the output table has the necessary attributes.
-        np.assertListEqual(list(actual_candset.columns.values),
+        self.assertListEqual(list(actual_candset.columns.values),
                           expected_output_attrs)
 
         actual_pairs = set()
@@ -353,9 +353,9 @@ class FilterTablesTestCases(unittest.TestCase):
                                        str(row[r_out_prefix + args[3]]))))
 
         # verify whether the actual pairs and the expected pairs match.
-        np.assertEqual(len(expected_pairs), len(actual_pairs))
+        self.assertEqual(len(expected_pairs), len(actual_pairs))
         common_pairs = actual_pairs.intersection(expected_pairs)
-        np.assertEqual(len(common_pairs), len(expected_pairs))
+        self.assertEqual(len(common_pairs), len(expected_pairs))
 
 
 # test SizeFilter.filter_candset method
@@ -444,7 +444,7 @@ class FilterCandsetTestCases(unittest.TestCase):
         actual_output_candset = size_filter.filter_candset(*args)
 
         # verify whether the output table has the necessary attributes.
-        np.assertListEqual(list(actual_output_candset.columns.values),
+        self.assertListEqual(list(actual_output_candset.columns.values),
                           list(args[0].columns.values))
 
         actual_pairs = set()
@@ -452,9 +452,9 @@ class FilterCandsetTestCases(unittest.TestCase):
             actual_pairs.add(','.join((str(row[args[1]]), str(row[args[2]]))))
 
         # verify whether the actual pairs and the expected pairs match.
-        np.assertEqual(len(expected_pairs), len(actual_pairs))
+        self.assertEqual(len(expected_pairs), len(actual_pairs))
         common_pairs = actual_pairs.intersection(expected_pairs)
-        np.assertEqual(len(common_pairs), len(expected_pairs))
+        self.assertEqual(len(common_pairs), len(expected_pairs))
 
 
 class SizeFilterInvalidTestCases(unittest.TestCase):
