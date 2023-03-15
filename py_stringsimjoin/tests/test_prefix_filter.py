6398ca75db8dba9f6a5f7614832be04713e76ea1
Anson-Doan <anson.ddoan@gmail.com>
2023-03-15 17:46:34 -0500

    Update test_prefix_filter.py

--- py_stringsimjoin/tests/test_prefix_filter.py
+++ py_stringsimjoin/tests/test_prefix_filter.py
@@ -119,7 +119,7 @@ class FilterPairTestCases(unittest.TestCase):
         prefix_filter = PrefixFilter(tokenizer, sim_measure_type, threshold,
                                      allow_empty, allow_missing)
         actual_output = prefix_filter.filter_pair(lstring, rstring)
-        np.assertEqual(actual_output, expected_output)
+        self.assertEqual(actual_output, expected_output)
 
 
 # test PrefixFilter.filter_tables method
@@ -335,7 +335,7 @@ class FilterTablesTestCases(unittest.TestCase):
                     expected_output_attrs.append(r_out_prefix + attr)
 
         # verify whether the output table has the necessary attributes.
-        np.assertListEqual(list(actual_candset.columns.values),
+        self.assertListEqual(list(actual_candset.columns.values),
                           expected_output_attrs)
 
         actual_pairs = set()
@@ -344,9 +344,9 @@ class FilterTablesTestCases(unittest.TestCase):
                                        str(row[r_out_prefix + args[3]]))))
 
         # verify whether the actual pairs and the expected pairs match.
-        np.assertEqual(len(expected_pairs), len(actual_pairs))
+        self.assertEqual(len(expected_pairs), len(actual_pairs))
         common_pairs = actual_pairs.intersection(expected_pairs)
-        np.assertEqual(len(common_pairs), len(expected_pairs))
+        self.assertEqual(len(common_pairs), len(expected_pairs))
 
 
 # test PrefixFilter.filter_candset method
@@ -434,7 +434,7 @@ class FilterCandsetTestCases(unittest.TestCase):
         actual_output_candset = prefix_filter.filter_candset(*args)
 
         # verify whether the output table has the necessary attributes.
-        np.assertListEqual(list(actual_output_candset.columns.values),
+        self.assertListEqual(list(actual_output_candset.columns.values),
                           list(args[0].columns.values))
 
         actual_pairs = set()
@@ -442,9 +442,9 @@ class FilterCandsetTestCases(unittest.TestCase):
             actual_pairs.add(','.join((str(row[args[1]]), str(row[args[2]]))))
 
         # verify whether the actual pairs and the expected pairs match.
-        np.assertEqual(len(expected_pairs), len(actual_pairs))
+        self.assertEqual(len(expected_pairs), len(actual_pairs))
         common_pairs = actual_pairs.intersection(expected_pairs)
-        np.assertEqual(len(common_pairs), len(expected_pairs))
+        self.assertEqual(len(common_pairs), len(expected_pairs))
 
 
 class PrefixFilterInvalidTestCases(unittest.TestCase):
